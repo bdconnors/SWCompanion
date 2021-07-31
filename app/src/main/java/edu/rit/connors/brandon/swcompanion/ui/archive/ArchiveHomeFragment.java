c85343f79f5365ea0e88595c529a8afe325e601a
bdconnors <bdc5435@rit.edu>
2021-07-31 15:06:51 -0400

    changed home fragments to tabs instead of grid

--- app/src/main/java/edu/rit/connors/brandon/swcompanion/ui/archive/ArchiveHomeFragment.java
+++ app/src/main/java/edu/rit/connors/brandon/swcompanion/ui/archive/ArchiveHomeFragment.java
@@ -1,45 +1,29 @@
 package edu.rit.connors.brandon.swcompanion.ui.archive;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-
-import java.util.ArrayList;
-
-import edu.rit.connors.brandon.swcompanion.R;
-import edu.rit.connors.brandon.swcompanion.network.source.DataSource;
+import edu.rit.connors.brandon.swcompanion.network.service.ArchiveService;
 import edu.rit.connors.brandon.swcompanion.network.source.starwars.StarWars;
 import edu.rit.connors.brandon.swcompanion.network.source.wookieepedia.Wookieepedia;
-import edu.rit.connors.brandon.swcompanion.ui.sources.SWOfficialArchive;
-import edu.rit.connors.brandon.swcompanion.ui.sources.WookieepediaArchive;
-import edu.rit.connors.brandon.swcompanion.ui.util.adapter.GridListAdapter;
+import edu.rit.connors.brandon.swcompanion.ui.util.fragment.TabPagerFragment;
 
-public class ArchiveHomeFragment extends Fragment {
+public class ArchiveHomeFragment extends TabPagerFragment {
 
-    protected GridView gridView;
-    protected GridListAdapter<DataSource> adapter;
-    protected Fragment[] fragments;
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.grid_list_fragment, container, false);
-        fragments = new Fragment[]{
-                new WookieepediaArchive(),
-                new SWOfficialArchive()
-        };
-        gridView = view.findViewById(R.id.grid_view);
-        adapter = new ArchiveHomeAdapter(getActivity(), fragments);
-        gridView.setAdapter(adapter);
-        adapter.setItems(new ArrayList<DataSource>(){{
-            add(new Wookieepedia());
-            add(new StarWars());
-        }});
-        return view;
+    private static final Wookieepedia wookieepedia = new Wookieepedia();
+    private static final StarWars starWars = new StarWars();
+
+    private static final ArchiveService wookiepeediaService = new ArchiveService(wookieepedia);
+    private static final ArchiveService starWarsService = new ArchiveService(starWars);
+
+    protected static final Fragment[] pageFragments = new Fragment[]{
+            new ArchiveGridFragment(wookiepeediaService, 0),
+            new ArchiveGridFragment(starWarsService, 1)
+    };
+    protected static final String[] pageTitles = new String[]{"SW Official", "Wookieepedia"};
+
+
+    public ArchiveHomeFragment() {
+        super(pageTitles, pageFragments);
     }
 }
--- app/src/main/java/edu/rit/connors/brandon/swcompanion/ui/holonet/HoloNetHomeFragment.java
+++ app/src/main/java/edu/rit/connors/brandon/swcompanion/ui/holonet/HoloNetHomeFragment.java
@@ -1,39 +1,23 @@
 package edu.rit.connors.brandon.swcompanion.ui.holonet;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.ArrayList;
-
-import edu.rit.connors.brandon.swcompanion.R;
-import edu.rit.connors.brandon.swcompanion.network.source.DataSource;
+import edu.rit.connors.brandon.swcompanion.network.service.ArchiveService;
 import edu.rit.connors.brandon.swcompanion.network.source.starwars.StarWars;
-import edu.rit.connors.brandon.swcompanion.ui.sources.SWOfficialHoloNet;
-import edu.rit.connors.brandon.swcompanion.ui.util.adapter.GridListAdapter;
+import edu.rit.connors.brandon.swcompanion.ui.util.fragment.TabPagerFragment;
 
-public class HoloNetHomeFragment extends Fragment {
+public class HoloNetHomeFragment extends TabPagerFragment {
 
-    protected GridView gridView;
-    protected GridListAdapter<DataSource> adapter;
-    protected Fragment[] fragments;
+    private static final StarWars starWars = new StarWars();
+    private static final ArchiveService starWarsService = new ArchiveService(starWars);
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.grid_list_fragment, container, false);
-        fragments = new Fragment[]{ new SWOfficialHoloNet() };
-        gridView = view.findViewById(R.id.grid_view);
-        adapter = new HoloNetHomeAdapter(getActivity(), fragments);
-        gridView.setAdapter(adapter);
-        adapter.setItems(new ArrayList<DataSource>(){{
-            add(new StarWars()); }});
-        return view;
+    protected static final String[] pageTitles = new String[]{"SW Official"};
+    protected static final Fragment[] pageFragments = new Fragment[]{
+            new HoloNetListFragment(starWarsService,0)
+    };
+
+    public HoloNetHomeFragment() {
+        super(pageTitles, pageFragments);
     }
 
 }
